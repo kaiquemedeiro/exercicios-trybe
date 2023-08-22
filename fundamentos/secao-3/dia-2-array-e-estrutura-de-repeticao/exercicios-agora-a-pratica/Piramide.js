b6494fd390ca7d4ba322764959021fc993edb449
Kaique Medeiro <kaique.medeiro14@hotmail.com>
2023-08-22 10:41:26 -0300

    realizado topicos -estrutrura de repeticao e exercicios pratica-

---
+++ fundamentos/secao-3/dia-2-array-e-estrutura-de-repeticao/exercicios-agora-a-pratica/Piramide.js
@@ -0,0 +1,9 @@
+let n = 10;
+
+for (let index = 0; index < n; index += 1) {
+    for (let index = 0; index < n; index += 1) {
+        process.stdout.write(`*`);
+    }
+    console.log('');   
+}
+

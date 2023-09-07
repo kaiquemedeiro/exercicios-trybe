db5feadfe898e2ffaae30b63dd870e8729750587
Kaique Medeiro <kaique.medeiro14@hotmail.com>
2023-09-07 14:13:51 -0300

    finalizado exercicio bonus

--- fundamentos/secao-5/dia-2-bibliotecas-javascript-eframeworks-css/vamos-praticar/script.js
+++ fundamentos/secao-5/dia-2-bibliotecas-javascript-eframeworks-css/vamos-praticar/script.js
@@ -1,4 +1,42 @@
-import JustValidate from "just-validate";
+import JustValidate from "/package.json";
 
-const validator = new JustValidate;
+const validator = new JustValidate();
 
+
+
+validator.addField(document.querySelector('#fullName'), [
+    {
+        rule: 'minlength',
+        value: 10,
+    },
+    {
+        rule: 'maxlength',
+        value: 40,
+    }
+]).addField(document.querySelector('#email'), [
+    {
+        rule: 'minlength',
+        value: 10,
+    },
+    {
+        rule: 'maxlength',
+        value: 50,
+    }
+]).addField(document.querySelector('#question') [
+    {
+        rule: 'cols',
+        value: 30,
+    },
+    {
+        rule: 'rows',
+        value: 10,
+    },
+    {
+        rule: 'minlength',
+        value: 1,
+    },
+    {
+        rule: 'maxlength',
+        value: 500,
+    }
+]);

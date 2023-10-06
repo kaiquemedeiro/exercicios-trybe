29cdba81b898ad511d9fac55873b0e4d1128ee1e
Kaique Medeiro <kaique.medeiro14@hotmail.com>
2023-10-06 17:04:57 -0300

    realizado css e terminado exercicio

--- front-end/secao-1/dia-1-ambiente-de-desenvolvimento/exercicios-agora-a-pratica/src/main.js
+++ front-end/secao-1/dia-1-ambiente-de-desenvolvimento/exercicios-agora-a-pratica/src/main.js
@@ -1,3 +1,29 @@
 import validator from 'validator';
 
-console.log(validator.isEmail('foo@bar.com'));
+import './style.css';
+
+const BtnElement = document.querySelector('button');
+
+BtnElement.addEventListener('click', () => {
+
+    const inputElement = document.querySelector('input');
+
+    const inputValue = inputElement.value;
+
+    const h3element = document.querySelector('h3');
+
+    const selectValue = document.querySelector('#select-types').value;
+
+    const objValidation = {
+        email: validator.isEmail(inputValue),
+        cpf: validator.isTaxID(inputValue, 'pt-BR')
+    };
+
+    const resultValidation = objValidation[selectValue];
+
+    h3element.innerText = `A validação retornou ${resultValidation}`;
+});
+
+
+
+
